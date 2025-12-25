017c17dab85027ad77f3e38f980cc395fefe3afe
DrRizzler <incompetent_sys_admin@protonmail.com>
2025-12-24 17:33:12 -0800

    Delete bb directory

--- bb/assets/src/config.py
+++
@@ -1,4 +0,0 @@
-FPS = 60
-BG = (100,80,100)
-width,height = 800,600
-RES = (width,height)
--- bb/main.py
+++
@@ -1,6 +0,0 @@
-#NerdB01/Baudm0n
-from assets.src.game import GAME
-
-if __name__ == "__main__":
-    main = GAME()
-    main.Loop()
